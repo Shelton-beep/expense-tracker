f0abf942fc7e88ba819bc7b97067575859f15b6f
shelton <shltnsimbi@gmail.com>
2023-05-31 16:31:47 +0200

    Added sideBar icons

--- src/components/Navs/SideBar.tsx
+++ src/components/Navs/SideBar.tsx
@@ -1,13 +1,31 @@
 import "./SideBar.css";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import DashboardCustomizeRoundedIcon from "@mui/icons-material/DashboardCustomizeRounded";
+import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
 
 export const SideBar = () => {
   return (
     <div className="sideBar">
       <div className="sideBarWrapper">
         <ul className="sideBarList">
-          <li className="sideBarListItem">Manage Expenses</li>
-          <li className="sideBarListItem">View Dashboards</li>
-          <li className="sideBarListItem">Manage Revenue</li>
+          <li className="sideBarListItem">
+            <span className="sideBarIcon">
+              <ShoppingCartRoundedIcon />
+            </span>
+            Manage Expenses
+          </li>
+          <li className="sideBarListItem">
+            <span className="sideBarIcon">
+              <DashboardCustomizeRoundedIcon />
+            </span>
+            View Dashboards
+          </li>
+          <li className="sideBarListItem">
+            <span className="sideBarIcon">
+              <AttachMoneyRoundedIcon />
+            </span>
+            Manage Revenue
+          </li>
           <li className="sideBarListItem">item4</li>
           <li className="sideBarListItem">item5</li>
           <li className="sideBarListItem">item6</li>
--- src/components/Navs/TopBar.tsx
+++ src/components/Navs/TopBar.tsx
@@ -1,17 +1,22 @@
 export const TopBar = () => {
   return (
     <>
-      <nav className="navbar sticky-top bg-body-tertiary">
+      <nav className="navbar sticky-top bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
           <a className="navbar-brand">Expenses Tracker</a>
           <form className="d-flex" role="search">
             <input
               className="form-control me-2"
+              data-bs-theme="light"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button
+              className="btn btn-outline-success"
+              data-bs-theme="light"
+              type="submit"
+            >
               Search
             </button>
           </form>
