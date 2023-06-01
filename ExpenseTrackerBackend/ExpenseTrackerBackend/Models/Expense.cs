9cfdf55e267106fe84eca46d606154849f9807fb
shelton <shltnsimbi@gmail.com>
2023-06-01 12:39:12 +0200

    Added Backend

---
+++ ExpenseTrackerBackend/ExpenseTrackerBackend/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerBackend.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
